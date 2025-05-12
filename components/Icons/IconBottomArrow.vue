d9c30dac601423af7d896731c5c8a4bc0d2dfa39
Linkolnn <rozarioa1974@gmail.com>
2025-05-12 23:28:05 +0500

    Адатпирование под ебанные Iphone 1.1

---
+++ components/Icons/IconBottomArrow.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg 
+    :width="size" 
+    :height="size" 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    xmlns="http://www.w3.org/2000/svg"
+    :stroke="color"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path 
+      d="M6 9L12 15L18 9" 
+      :fill="filled ? color : 'none'" 
+      :stroke="color"
+    />
+  </svg>
+</template>
+
+<script setup>
+const props = defineProps({
+  size: {
+    type: [Number, String],
+    default: 24
+  },
+  color: {
+    type: String,
+    default: 'currentColor'
+  },
+  filled: {
+    type: Boolean,
+    default: false
+  }
+});
+</script>
